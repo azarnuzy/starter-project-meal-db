1c160a84008f507bbafcea866c7343f8ef6a7d9e
azarnuzy <azarnuzy@gmail.com>
2023-10-21 19:41:27 +0700

    add tailwind configuration

---
+++ apps/recipe-app/tailwind.config.js
@@ -0,0 +1,23 @@
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+const { join } = require('path');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    join(
+      __dirname,
+      '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'
+    ),
+    ...createGlobPatternsForDependencies(__dirname),
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#F4D8D2',
+        secondary: '#48d0ce',
+        danger: '#ee4d64',
+      },
+    },
+  },
+  plugins: [],
+};
